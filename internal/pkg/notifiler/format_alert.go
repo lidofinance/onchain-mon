ebc69392e66b2ad03e8379da1f22e64493567ba8
Artyom Veremeenko <arwer13@gmail.com>
2024-10-08 22:58:02 +0300

    feat(forwarder): 1st attempt to make footer compact

--- internal/pkg/notifiler/format_alert.go
+++ internal/pkg/notifiler/format_alert.go
@@ -19,22 +19,15 @@ func FormatAlert(alert *databus.FindingDtoJson, source string) string {
 		footer += "\n\n"
 	}
 
-	footer += fmt.Sprintf("Alert Id: %s", alert.AlertId)
-	footer += fmt.Sprintf("\nBot name: %s", alert.BotName)
-	footer += fmt.Sprintf("\nTeam: %s", alert.Team)
-
+	quorumTime := time.Now()
 	if alert.BlockNumber != nil {
-		footer += fmt.Sprintf("\nBlock number: [%d](https://etherscan.io/block/%d/)", *alert.BlockNumber, *alert.BlockNumber)
+		eventToQuorumSecs := int(quorumTime.Unix()) - *alert.BlockTimestamp
+		footer += fmt.Sprintf("Happened at block [%d](https://etherscan.io/block/%d/), ~ %d seconds ago", *alert.BlockNumber, *alert.BlockNumber, eventToQuorumSecs)
 	}
+	footer += fmt.Sprintf("\nTeam %s | %s | %s | quorum at %s by %s", alert.Team, alert.BotName, alert.AlertId, quorumTime.Format("15:04:05.000 MST"), source)
 
 	if alert.TxHash != nil {
 		footer += fmt.Sprintf("\nTx hash: [%s](https://etherscan.io/tx/%s/)", shortenHex(*alert.TxHash), *alert.TxHash)
-	}
-
-	footer += fmt.Sprintf("\nSource: %s", source)
-	footer += fmt.Sprintf("\nServer timestamp: %s", time.Now().Format("15:04:05.000 MST"))
-	if alert.BlockTimestamp != nil {
-		footer += fmt.Sprintf("\nBlock timestamp:   %s", time.Unix(int64(*alert.BlockTimestamp), 0).Format("15:04:05.000 MST"))
 	}
 
 	return fmt.Sprintf("%s%s", body, footer)
