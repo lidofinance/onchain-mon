eedbdcc562ba8b157bd762872f420f0a0588c14a
Sergey Wh1te <sergey.i@lido.fi>
2025-08-18 16:12:50 +0300

    feat: 1. Update to go1.25       2. Update dependencies       3. Fix linters, format warnings

--- internal/app/feeder/feeder.go
+++ internal/app/feeder/feeder.go
@@ -16,10 +16,9 @@ import (
 	"github.com/nats-io/nats.go/jetstream"
 	"github.com/prometheus/client_golang/prometheus"
 
-	"github.com/lidofinance/onchain-mon/internal/pkg/chain"
-
 	"github.com/lidofinance/onchain-mon/generated/databus"
 	"github.com/lidofinance/onchain-mon/internal/connectors/metrics"
+	"github.com/lidofinance/onchain-mon/internal/pkg/chain"
 	"github.com/lidofinance/onchain-mon/internal/pkg/chain/entity"
 )
 
